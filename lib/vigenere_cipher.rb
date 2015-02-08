05cac92fd737e143e78f5f4eadda2bee8a0e29c4
Erik Griffin <griffin.erik@gmail.com>
2015-02-08 20:09:38 +0000

    Got passphrase to repeat over message text

--- lib/vigenere_cipher.rb
+++ lib/vigenere_cipher.rb
@@ -1,4 +1,28 @@
 class VigenereCipher
 
+  attr_accessor :message
+  attr_accessor :phrase
+
+  def initialize
+    @char_array = [*'a'..'z']
+  end
+
+  def input(message)
+    @message = message.downcase
+  end
+
+  def use_password(phrase)
+    @phrase = ""
+    i = 0
+    @message.chars.each do |x|
+      if x == " "
+        @phrase << " "
+      else
+        @phrase << phrase.upcase[i]
+        i += 1
+        i = i%phrase.length
+      end
+  end
+
 
 end
--- spec/vigenere_cipher_spec.rb
+++ spec/vigenere_cipher_spec.rb
@@ -1,6 +1,23 @@
-require 'vigenerecipher'
+require 'vigenere_cipher'
 
 describe 'VigenereCipher' do
+
+  let(:cipher) {VigenereCipher.new}
+  let(:message) {"Attack at dawn"}
+  let(:password) {"FREEDOM"}
+
+  it 'should take an input string and a passphrase' do
+    cipher.input(message)
+    cipher.use_password(password)
+
+  end
+
+  it 'passphrase should repeat for every non-" " character' do
+    cipher.input(message)
+    cipher.use_password("FREEDOM")
+    expect(cipher.phrase).to eq "FREEDO MF REED"
+  end
+
 
 
 

e68e70b48d92c9f5b80c9ff4a3f04728b8a0873a
Erik Griffin <griffin.erik@gmail.com>
2015-02-08 23:55:25 +0000

    Close. Some bug is causing certain characters to be nil, destroying the result

--- lib/vigenere_cipher.rb
+++ lib/vigenere_cipher.rb
@@ -4,14 +4,73 @@ class VigenereCipher
   attr_accessor :phrase
 
   def initialize
-    @char_array = [*'a'..'z']
+    @lo_char_array = [*'a'..'z']
+    @hi_char_array = [*'A'..'Z']
   end
+
+  def alphanumeric?(input)
+    if input.match /[^a-zA-Z0-9]/
+      return false
+    else
+      return true
+    end
+  end
+
 
   def input(message)
     @message = message.downcase
   end
 
   def use_password(phrase)
+    alphanumeric?(phrase) ? @phrase = phrase : raise('ERROR: Non-alphanumeric pass')
+  end
+
+  def ready?
+    if message
+      lengthen_password(@phrase)
+      return true
+    else
+      raise 'No message to encrypt.'
+    end
+  end
+
+  def encrypt
+    if ready?
+      index = 0
+      encrypted_message = ""
+      @message.chars.map do |x|
+
+        if @lo_char_array.include? x
+          x = @lo_char_array[charnum(x)+charnum(@phrase[index])%26]
+        else
+          if @hi_char_array.include? x
+            x = @hi_char_array[charnum(x)+charnum(@phrase[index])%26]
+          else
+            x
+          end
+        end
+        if x
+          puts x
+          encrypted_message << x
+        else
+          puts x
+        end
+
+        index += 1
+      end # map |x|
+      puts @message
+
+      encrypted_message
+    end # if ready?
+  end
+
+  def charnum(char)
+    @lo_char_array.index(char.downcase)
+  end
+
+
+
+  def lengthen_password(phrase)
     @phrase = ""
     i = 0
     @message.chars.each do |x|
@@ -22,6 +81,7 @@ class VigenereCipher
         i += 1
         i = i%phrase.length
       end
+    end
   end
 
 
--- spec/vigenere_cipher_spec.rb
+++ spec/vigenere_cipher_spec.rb
@@ -14,9 +14,29 @@ describe 'VigenereCipher' do
 
   it 'passphrase should repeat for every non-" " character' do
     cipher.input(message)
-    cipher.use_password("FREEDOM")
+    cipher.lengthen_password("FREEDOM")
     expect(cipher.phrase).to eq "FREEDO MF REED"
   end
+
+  it 'should reject passphrases that are not alpha-numeric' do
+    input = 'Bad-phrase'
+    expect{cipher.use_password(input)}.to raise_error 'ERROR: Non-alphanumeric pass'
+  end
+
+  it 'should shift each letter of the message by the appropriate amount' do
+
+    #         FREEDO MF REED
+    #         Attack at dawn
+    #         ==============
+    result = 'fkxefy my ueaq'
+
+    cipher.input(message)
+    cipher.use_password(password)
+    expect(cipher.encrypt).to eq result
+
+  end
+
+
 
 
 
